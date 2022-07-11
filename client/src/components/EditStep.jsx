0f2f3c9400a0c11d48eee651ebfbaae46f076f12
Manuel Casanova <manucasanova@hotmail.com>
2022-07-11 12:46:28 -0700

    Work in progress. Still does not refresh with the edited step

--- client/src/components/EditStep.jsx
+++ client/src/components/EditStep.jsx
@@ -1,27 +1,57 @@
 import { useState } from "react"
+import StepList from "./StepList";
 
 const URL = 'http://localhost:8001'
 
-export default function EditStep({ step, levels }) {
+export default function EditStep({ step, levels, steps, setSteps }) {
 
   const [stepTitle, setStepTitle] = useState(step.steptitle);
   const [stepLevel_id, setStepLevel_id] = useState(step.steplevel_id);
   // const [stepPronunciation, setStepPronunciation] = useState(step.steppronunciation);
   // const [stepVideo, setStepVideo] = useState(step.stepvideo);
   // const [stepVideoBreakdown, setStepVideoBreakdown] = useState(step.stepvideobreakdown);
-  
+
 
   //This edit function is called from the onClick on edit button
 
-  const editStep = async(e) => {
+  const editStep = async (e) => {
     e.preventDefault()
     try {
-      const body = {stepTitle, stepLevel_id};
+      const body = { stepTitle, stepLevel_id };
       const response = await fetch(`${URL}/steps/${step.stepid}`, {
         method: "PUT",
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
       })
+
+      //Prints the array of objects containing the steps from the db
+      console.log("steps", steps)
+
+      //Creates a copy of the edited step
+      const editedStep = body;
+      console.log("editedstep", editedStep)
+
+      //FIND: Would return the value of the first element that does not match the id. But we want all of them. Similar to map. Then we probably need FILTER. Gets all the elements in the array that do not match the id.
+
+      //I believe this should return all the steps from the db except the one edited. But it return all of them.
+
+      const filteredSteps = steps.filter(step => step.stepLevel_id !== editedStep.stepLevel_id)
+
+      // const filteredSteps = steps.filter((step) => {
+      //   return step.stepLevel_id !== editedStep.stepLevel_id
+      // })
+
+      console.log("filteredSteps", filteredSteps)
+
+
+
+      // setSteps(list of steps in the db without the new step + edited step)
+
+   
+
+
+
+
       console.log(response)
     } catch (err) {
       console.error(err.message)
@@ -30,64 +60,64 @@ export default function EditStep({ step, levels }) {
 
   return (
     <div className='EditStepModal'>
-            {/* Button to Open the Modal */}
-            <button type="button" className="BUTTON_EDIT" data-toggle="modal" data-target={`#id${step.stepid}`}>
-      Edit
-    </button>
-    
-     {/* The Modal */}
-    <div className="modal" id={`id${step.stepid}`}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-    
-           {/* Modal Header */}
-          <div className="modal-header">
-            <h4 className="modal-title">Modal Heading</h4>
-            <button type="button" className="close" data-dismiss="modal">&times;</button>
-          </div>
-    
-           {/* Modal body */}
-           <label className="modal-body" htmlFor="title">Step</label>
-          <div className="modal-body">
-            <input type="text" 
-            className="form-control" 
-            value={stepTitle}
-            onChange={e => setStepTitle(e.target.value)}
-             />
-          </div>
+      {/* Button to Open the Modal */}
+      <button type="button" className="BUTTON_EDIT" data-toggle="modal" data-target={`#id${step.stepid}`}>
+        Edit
+      </button>
 
-          <label className="add_step_title" htmlFor="title">Level</label>
-          <div className="modal-body">
-            <select
-              className="form-control"
-              value={stepLevel_id}
-              //Change the stepLevel state. Then on clicking on the Edit button at the modal footer, send the information
-              onChange={e => setStepLevel_id(e.target.value)} >
+      {/* The Modal */}
+      <div className="modal" id={`id${step.stepid}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
 
-{/* {console.log("steplevelid after change", stepLevelId)} */}
+            {/* Modal Header */}
+            <div className="modal-header">
+              <h4 className="modal-title">Modal Heading</h4>
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+            </div>
 
-              {levels.map((level) =>
-                <option 
-                key={level.levelid}
-                value={level.levelid}>
-                  
-                  {level.leveltitle}
-                </option>
-              )}
-            </select>
+            {/* Modal body */}
+            <label className="modal-body" htmlFor="title">Step</label>
+            <div className="modal-body">
+              <input type="text"
+                className="form-control"
+                value={stepTitle}
+                onChange={e => setStepTitle(e.target.value)}
+              />
+            </div>
+
+            <label className="add_step_title" htmlFor="title">Level</label>
+            <div className="modal-body">
+              <select
+                className="form-control"
+                value={stepLevel_id}
+                //Change the stepLevel state. Then on clicking on the Edit button at the modal footer, send the information
+                onChange={e => setStepLevel_id(e.target.value)} >
+
+                {/* {console.log("steplevelid after change", stepLevelId)} */}
+
+                {levels.map((level) =>
+                  <option
+                    key={level.levelid}
+                    value={level.levelid}>
+
+                    {level.leveltitle}
+                  </option>
+                )}
+              </select>
+            </div>
+
+            {/* Modal footer */}
+            <div className="modal-footer">
+              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal"
+                onClick={e => editStep(e)}
+              >Edit</button>
+            </div>
+
           </div>
-    
-           {/* Modal footer */}
-          <div className="modal-footer">
-            <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-danger" data-dismiss="modal"
-            onClick={e => editStep(e)}
-            >Edit</button>
-          </div>
-    
         </div>
       </div>
-    </div>
     </div>
   )
 
