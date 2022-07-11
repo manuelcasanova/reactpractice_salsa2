bfc310e0c23164916dccf5b81b2dc60c840a20c9
Manuel Casanova <manucasanova@hotmail.com>
2022-07-11 07:32:42 -0700

    Route and query to edit step

--- server/index.js
+++ server/index.js
@@ -68,3 +68,18 @@ app.delete("/delete/:id", async (req, res) => {
     console.error(err.message)
   }
 })
+
+//Edit a step
+
+app.put("/steps/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      steptitle, steplevel_id, steppronunciation, stepvideo, stepvideobreakdown
+    } = req.body
+    const editStep = await pool.query('UPDATE steps SET steptitle = $1, steplevel_id = $2, steppronunciation = $3, stepvideo = $4, stepVideoBreakdown = $5 WHERE stepid = $6', [steptitle, steplevel_id, steppronunciation, stepvideo, stepvideobreakdown, id])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
