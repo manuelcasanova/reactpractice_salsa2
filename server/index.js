d8b29a467e450b7e1ce954d0bc9c2e1e22b4cc24
Manuel Casanova <manucasanova@hotmail.com>
2022-07-10 19:22:07 -0700

    Create server. Install dependencies express pg cors and nodemon

---
+++ server/index.js
@@ -0,0 +1,16 @@
+//The application will run on this file
+
+const express = require('express');
+const app = express();
+const port = 8001;
+const cors = require("cors");
+
+//const bodyparser = require("body-parser");//Does same as express.json
+
+app.use(cors());
+app.use(express.json()); //req.body
+
+app.listen(port, () => {
+  console.log(`Casino app running on port ${port}.`);
+});
+
