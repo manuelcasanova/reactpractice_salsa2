555c420b918beb0aa2c64af23d0050b05c329a48
Manuel Casanova <manucasanova@hotmail.com>
2022-07-10 19:50:22 -0700

    Import hooks useState and useEffect. Import axios. Declare variables steps and levels. UseEffect with axios get and set the state of both the steps and the levels with the response data

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,33 @@ import LevelList from './components/LevelList';
 import NewStep from './components/NewStep';
 import NewLevel from './components/NewLevel';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const URL = 'http://localhost:8001/'
+
 function App() {
+
+const [steps, setSteps] = useState([]);
+const [levels, setLevels] = useState([]);
+
+useEffect(() => {
+  axios.get(`${URL}steps`)
+  .then(function (res) {
+    setSteps([...res.data])
+    //console.log("axios res.data - steps State", res.data)
+  })
+}, [])
+
+
+useEffect(() => {
+  axios.get(`${URL}levels`)
+  .then(function (res) {
+    setLevels([...res.data])
+  })
+}, [])
+
   return (
     <div className="App">
       <StepList />
