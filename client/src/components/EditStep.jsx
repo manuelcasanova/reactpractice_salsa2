361b3d62cb2cc17d925d8ea16c309c927526d638
Manuel Casanova <manucasanova@hotmail.com>
2022-07-11 07:00:43 -0700

    Add modal to edit step

--- client/src/components/EditStep.jsx
+++ client/src/components/EditStep.jsx
@@ -3,9 +3,41 @@ export default function EditStep({ step }) {
 
   return (
     <div className='EditStepModal'>
-      Edit Step
+            {/* Button to Open the Modal */}
+            <button type="button" className="BUTTON_EDIT" data-toggle="modal" data-target="#myModal">
+      Edit
+    </button>
+    
+     {/* The Modal */}
+    <div className="modal" id="myModal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+    
+           {/* Modal Header */}
+          <div className="modal-header">
+            <h4 className="modal-title">Modal Heading</h4>
+            <button type="button" className="close" data-dismiss="modal">&times;</button>
+          </div>
+    
+           {/* Modal body */}
+          <div className="modal-body">
+            Modal body..
+          </div>
+    
+           {/* Modal footer */}
+          <div className="modal-footer">
+            <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-danger" data-dismiss="modal">Edit</button>
+          </div>
+    
+        </div>
+      </div>
+    </div>
     </div>
   )
 
 }
+
+
+
 
