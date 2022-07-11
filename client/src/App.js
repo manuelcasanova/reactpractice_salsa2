d824429a3862ca2f67f3eea6185e2ffe535686d0
Manuel Casanova <manucasanova@hotmail.com>
2022-07-10 19:45:53 -0700

    Import components and show them

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import StepList from './components/StepList';
+import LevelList from './components/LevelList';
+import NewStep from './components/NewStep';
+import NewLevel from './components/NewLevel';
 
 function App() {
   return (
     <div className="App">
-
+      <StepList />
+      <LevelList />
+      <NewStep />
+      <NewLevel />
     </div>
   );
 }
