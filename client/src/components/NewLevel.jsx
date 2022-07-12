5c3a23865ce1dc6cc81c72cb0d35c72e77a3049b
Manuel Casanova <manucasanova@hotmail.com>
2022-07-11 18:46:32 -0700

    Add form to add level. Add button. Add onClick event. Add onSubmit form. Add function addLevel and resetform

--- client/src/components/NewLevel.jsx
+++ client/src/components/NewLevel.jsx
@@ -1,7 +1,84 @@
-export default function NewLevel () {
+import axios from "axios";
+import { useState } from "react"
+
+export default function NewLevel({levels, setLevels}) {
+
+  const URL = 'http://localhost:8001'
+
+  const [levelTitle, setLevelTitle] = useState("")
+  const [levelDescription, setLevelDescription] = useState("")
+
+
+  function onSubmitForm(e) {
+    e.preventDefault();
+    const level = {
+      levelTitle,
+      levelDescription
+    }
+    addLevel(level);
+    resetForm();
+  }
+
+  function addLevel(level) {
+    console.log("Level added:", level)
+
+    return axios.post(`${URL}/levels`, level)
+      .then((response) => {
+        const newLevel = response.data;
+     
+        setLevels([newLevel, ...levels])
+      })
+  
+  }
+
+  function resetForm() {
+    setLevelTitle("");
+    setLevelDescription("");
+  }
+
+
   return (
-    <div>
-      New Level
-    </div>
+    <>
+
+      <div>
+        New Level form
+      </div>
+
+      <div className="newlevelform">
+
+        <form>
+
+
+          <div>Level name</div>
+          <input
+            type="text"
+            value={levelTitle}
+            onChange={e => setLevelTitle(e.target.value)}
+          />
+
+          <div>Level description</div>
+          <input
+            type="text"
+            value={levelDescription}
+            onChange={e => setLevelDescription(e.target.value)}
+          />
+
+
+
+        </form>
+
+        <button
+          onClick={onSubmitForm}
+        >Add</button>
+
+        <div>levelTitle State: {levelTitle}</div>
+        <div>levelDescription State: {levelDescription}</div>
+
+      </div>
+
+    </>
+
+
+
   )
 }
