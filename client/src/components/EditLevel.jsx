187fb5836c6d6619e5fa7d9081f78cd529a851a4
Manuel Casanova <manucasanova@hotmail.com>
2022-07-12 07:28:18 -0700

    Pass level and setLevels as props. Import axios and useState. Declare levelTitle and levelDescription with useState. Create modal and connect to levelid. Create function editLevel

--- client/src/components/EditLevel.jsx
+++ client/src/components/EditLevel.jsx
@@ -1,7 +1,85 @@
+import { useState } from "react";
+import axios from 'axios'
+
 export default function EditLevel ({level, levels, setLevels}) {
+
+  const URL = 'http://localhost:8001'
+
+  const [levelTitle, setLevelTitle] = useState(level.leveltitle);
+  const [levelDescription, setLevelDescription] = useState(level.levelDescription);
+
+
+  const editLevel = async (e) => {
+    e.preventDefault()
+    try {
+      const body = {levelTitle, levelDescription };
+      const response = await fetch(`${URL}/levels/${level.levelid}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      })
+      axios.get(`${URL}/levels`)
+      .then(function (res) {
+        setLevels([...res.data])
+      })
+      console.log(response)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+
+
   return (
-    <div>
-      Edit Level
+    <div className='EditLevelModal'>
+      {/* Button to Open the Modal */}
+      <button type="button" className="BUTTON_EDIT" data-toggle="modal" data-target={`#id${level.levelid}`}>
+        Edit
+      </button>
+
+      {/* The Modal */}
+      <div className="modal" id={`id${level.levelid}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            {/* Modal Header */}
+            <div className="modal-header">
+              <h4 className="modal-title">Edit level</h4>
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            {/* Modal body */}
+            <label className="modal-body" htmlFor="title">Level</label>
+            <div className="modal-body">
+              <input type="text"
+                className="form-control"
+                value={levelTitle}
+                onChange={e => setLevelTitle(e.target.value)}
+              />
+            </div>
+
+            <label className="modal-body" htmlFor="title">Description</label>
+            <div className="modal-body">
+              <input type="text"
+                className="form-control"
+                value={levelDescription}
+                onChange={e => setLevelDescription(e.target.value)}
+              />
+            </div>
+
+
+
+            {/* Modal footer */}
+            <div className="modal-footer">
+              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal"
+                onClick={e => editLevel(e)}
+              >Edit</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
