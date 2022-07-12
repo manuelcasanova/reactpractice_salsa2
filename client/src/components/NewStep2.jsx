2348ea4420738209ba3dad83d02ebfdefe9ee471
Manuel Casanova <manucasanova@hotmail.com>
2022-07-12 13:24:45 -0700

    New step form now in the page, not a modal

--- client/src/components/NewStep2.jsx
+++ client/src/components/NewStep2.jsx
@@ -3,13 +3,10 @@ import axios from "axios";
 
 export default function NewStep({ steps, setSteps, levels }) {
 
-//console.log(levels[0].leveltitle);
-
   const URL = 'http://localhost:8001'
 
   const [stepTitle, setStepTitle] = useState("");
   const [stepLevel, setStepLevel] = useState(1);
-  //const [stepLevel, setStepLevel] = useState({levels[0].leveltitle} || 1);
   const [stepPronunciation, setStepPronunciation] = useState("");
   const [stepVideo, setStepVideo] = useState("");
   const [stepVideoBreakdown, setStepVideoBreakdown] = useState("");
@@ -60,38 +57,25 @@ export default function NewStep({ steps, setSteps, levels }) {
   }
 
   return (
-    <div>
+    <>
+      <div>New step form</div>
+      <div className="newstepform">
 
-      {/* <!-- Button trigger modal --> */}
-      <button type="button" 
-      className="button_add" data-toggle="modal" data-target="#exampleModal">Add step
-      </button>
+        <form>
 
-      {/* <!-- Modal --> */}
-      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Add new step</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
+          <div>Step name</div>
+          <input
+            type="text"
+            value={stepTitle}
+            onChange={e => setStepTitle(e.target.value)}
+          />
 
-              <label className="add_step_title" htmlFor="title">Step</label>
-
-              <input className="form-control" type="text" name="title" value={stepTitle} onChange={e => setStepTitle(e.target.value)} />
-              <p></p>
-
-              <div className="level_input">
-
-                <label className="add_step_title" htmlFor="title">Level</label>
-                <select className="form-control" value={stepLevel} onChange={e => setStepLevel(e.target.value)}  >
-             
-    
-
-                {levels.map((level) =>
+          <div>Step level</div>
+          <select
+            value={stepLevel}
+            onChange={e => setStepLevel(e.target.value)}
+          >
+            {levels.map((level) =>
               <option
                 key={level.levelid}
                 value={level.levelid} //HERE to show name of step but step is the id!
@@ -99,29 +83,41 @@ export default function NewStep({ steps, setSteps, levels }) {
                 {level.leveltitle}
 
               </option>
-                  )}
-                  {/*App breaks when I reload the page. Uncaught TypeError levels.map is not a function. I comment out and in lines 85 to 88 and works again. FIXED by removing string from levels setLeves useState("Beginner")*/}
-                </select>
+            )}
+          </select>
 
-              </div>
+          <div>Step pronunciation (Mp3)</div>
+          <input
+            type="text"
+            value={stepPronunciation}
+            onChange={e => setStepPronunciation(e.target.value)}
+          />
 
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="button_close" data-dismiss="modal">Close</button>
-              {/* <button type="button" className="btn btn-primary">Save</button> */}
-              <button
-                className="button_submit"
-                type="Submit"
-                onClick={onSubmitForm}
-                /*to close modal after submission */
-                data-dismiss="modal"
-              >Add step</button>
-            </div>
-          </div>
-        </div>
+          <div>Step video</div>
+          <input
+            type="text"
+            value={stepVideo}
+            onChange={e => setStepVideo(e.target.value)}
+          />
+
+          <div>Step video breakdown</div>
+          <input
+            type="text"
+            value={stepVideoBreakdown}
+            onChange={e => setStepVideoBreakdown(e.target.value)}
+          />
+
+        </form>
+
+        <button
+          onClick={onSubmitForm}
+          className="button_submit"
+        >Add</button>
+
       </div>
 
+    </>
 
-    </div>
   )
 }
+
