6deecc6abaf4cf7227bd6a8953c5f150325f2794
Manuel Casanova <manucasanova@hotmail.com>
2022-07-13 12:53:35 -0700

    New level modal

--- client/src/components/NewLevel2.jsx
+++ client/src/components/NewLevel2.jsx
@@ -38,45 +38,53 @@ export default function NewLevel({levels, setLevels}) {
 
 
   return (
-    <>
+    <div>
 
-      <div className="formtitle">
-        New Level form
+      {/* <!-- Button trigger modal --> */}
+      <button type="button"
+      className="button_add"
+     data-toggle="modal" data-target={`#newlevelmodal${levels.levelid}`}>Add level
+      </button>
+
+      {/* <!-- Modal --> */}
+      <div className="modal fade" id={`newlevelmodal${levels.levelid}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Add new level</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+
+              <label className="add_level_title" htmlFor="title">Level</label>
+
+              <input className="form-control" type="text" name="title" value={levelTitle} onChange={e => setLevelTitle(e.target.value)} />
+              <p></p>
+
+              <label className="add_level_title" htmlFor="title">Description</label>
+
+              <input className="form-control" type="text" name="title" value={levelDescription} onChange={e => setLevelDescription(e.target.value)} />
+            
+
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="button_close" data-dismiss="modal">Close</button>
+              {/* <button type="button" className="btn btn-primary">Save</button> */}
+              <button
+                className="button_submit"
+                type="Submit"
+                onClick={onSubmitForm}
+                /*to close modal after submission */
+                data-dismiss="modal"
+              >Add level</button>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="newlevelform">
 
-        <form>
-
-
-          <div className="formcolumn">Level name</div>
-          <input
-            type="text"
-            value={levelTitle}
-            onChange={e => setLevelTitle(e.target.value)}
-          />
-
-          <div className="formcolumn">Level description</div>
-          <input
-            type="text"
-            value={levelDescription}
-            onChange={e => setLevelDescription(e.target.value)}
-          />
-
-
-
-        </form>
-
-        <button
-          onClick={onSubmitForm}
-          className="button_submit"
-        >Add</button>
-
-      </div>
-
-    </>
-
-
-
+    </div>
   )
 }
